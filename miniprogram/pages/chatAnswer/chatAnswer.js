7701c533c5424479812bd7c4f5802b508d81e590
jeremylai7 <1049024592@qq.com>
2022-12-07 12:30:30 +0800

    添加文字回复

---
+++ miniprogram/pages/chatAnswer/chatAnswer.js
@@ -0,0 +1,110 @@
+// pages/chatAnswer/chatAnswer.js
+const app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    searchText:"",
+    showResult:false,
+    resultText:"",
+  },
+
+  changeText: function (e){
+    this.setData({
+      searchText: e.detail.value
+    })
+  },
+
+  chatAnswer() {
+    var that =  this;
+    var searchText = this.data.searchText;
+    if(searchText == "") {
+      wx.showToast({
+        title: "请输入文字",
+        icon: 'none',
+        duration: 2000
+      })
+      return;
+    }
+    wx.showLoading({
+      title: '请稍等',
+    })
+    wx.request({
+      url: app.globalData.httptype + app.globalData.url + "/holiday/chat-answer",
+      data: {
+         msg:searchText
+      },
+      success (res){
+        wx.hideLoading();
+        var url = res.data.choices[0].text;
+        that.setData({
+          resultText:url,
+          showResult:true
+        })
+        
+      },
+      fail (res) {
+        wx.hideLoading();
+      }
+      
+    })
+
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
