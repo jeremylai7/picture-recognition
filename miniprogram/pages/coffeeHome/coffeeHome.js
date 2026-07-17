d3a80971f53088fa4c263bee385b253549121594
laizc <1049024592@qq.com>
2026-07-17 10:25:42 +0800

    添加coffee

--- miniprogram/pages/coffeeHome/coffeeHome.js
+++ miniprogram/pages/coffeeHome/coffeeHome.js
@@ -2,6 +2,12 @@ Page({
   data: {
     statusBarHeight: 20,
     activeTab: 0,
+    activeMallCategory: 0,
+    currentHero: 0,
+    heroBanners: [
+      { image: '/images/coffee-sports-hero.jpg' },
+      { image: '/images/coffee-duolingo-hero-clean.jpg' }
+    ],
     primaryServices: [
       { icon: '☕', iconClass: 'icon-pickup', title: '到店取', subtitle: '下单即享优惠' },
       { icon: '⚡', iconClass: 'icon-delivery', title: '幸运送', subtitle: '美味快速送达' },
@@ -17,6 +23,49 @@ Page({
       { name: '生椰拿铁', price: '9.9', color: 'cup-blue' },
       { name: '橙C美式', price: '13.9', color: 'cup-orange' },
       { name: '酱香拿铁', price: '15.9', color: 'cup-brown' }
+    ],
+    mallCategories: ['瑞幸即享', '联名限定', '限时秒杀'],
+    mallProducts: [
+      {
+        image: '/images/coffee-mall-product-1.jpg',
+        title: '【热销榜单No.1】瑞幸醇香/经典浓缩咖啡液 32杯/盒',
+        tags: ['赠品'],
+        priceLabel: '秒杀价',
+        price: '64',
+        handPrice: '64',
+        sold: '已售卖763900+件',
+        limit: '限购6件'
+      },
+      {
+        image: '/images/coffee-mall-product-2.jpg',
+        title: '【海绵宝宝联名】冷萃黑咖啡液/果C美式咖啡液36杯/组',
+        tags: ['首购立减3元', '限时立减', '赠品'],
+        priceLabel: '',
+        price: '168',
+        handPrice: '101',
+        sold: '已售卖5100+件',
+        limit: '限购5件'
+      },
+      {
+        image: '/images/coffee-mall-product-3.jpg',
+        title: '瑞幸大师经典冻干咖啡 大颗粒黑咖啡（新升级90g/瓶）',
+        tags: ['限时立减', '满减', '赠品'],
+        priceLabel: '',
+        price: '59.9',
+        handPrice: '54.9',
+        sold: '已售卖75700+件',
+        limit: '限购10件'
+      },
+      {
+        image: '/images/coffee-mall-product-4.jpg',
+        title: '【60杯囤货装】瑞幸元气弹2.0系列即溶咖啡（2.3g/杯）',
+        tags: ['赠品'],
+        priceLabel: '秒杀价',
+        price: '169',
+        handPrice: '169',
+        sold: '已售卖117400+件',
+        limit: ''
+      }
     ],
     tabs: [
       { icon: '⌂', title: '首页' },
@@ -46,12 +95,29 @@ Page({
     wx.showToast({ title: '活动详情', icon: 'none' });
   },
 
+  changeHero(event) {
+    this.setData({ currentHero: event.detail.current });
+  },
+
   tapService(event) {
     wx.showToast({ title: event.currentTarget.dataset.title, icon: 'none' });
   },
 
   showPromotion() {
     wx.showToast({ title: '优惠活动', icon: 'none' });
+  },
+
+  switchMallCategory(event) {
+    this.setData({ activeMallCategory: Number(event.currentTarget.dataset.index) });
+  },
+
+  openMallProduct(event) {
+    const product = this.data.mallProducts[Number(event.currentTarget.dataset.index)];
+    wx.showToast({ title: product ? product.title.slice(0, 10) : '商品详情', icon: 'none' });
+  },
+
+  openMallMore() {
+    wx.showToast({ title: '更多即享商品', icon: 'none' });
   },
 
   switchTab(event) {
